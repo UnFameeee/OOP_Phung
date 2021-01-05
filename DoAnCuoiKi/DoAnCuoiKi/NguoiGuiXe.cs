8e92c4a68b8c9765bdaada110d82765058387879
Q.Thang <quocthang036@gmail.com>
2021-01-05 22:10:53 +0700

    NMD

---
+++ DoAnCuoiKi/DoAnCuoiKi/NguoiGuiXe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnCuoiKi
+{
+    public class NguoiGuiXe:Nguoi
+    {
+        public int theXe { set; get; }
+        public NguoiGuiXe():base()
+        {
+            this.theXe = -1;
+        }
+        public NguoiGuiXe(string khuonMat, GioiTinh gioiTinh, string dangNguoi, int theXe):base(khuonMat,gioiTinh,dangNguoi)
+        {
+            this.theXe = theXe;
+        }
+        public NguoiGuiXe(NguoiGuiXe x)
+        {
+            this.theXe = x.theXe;
+            this.khuonMat = x.khuonMat;
+            this.gioiTinh = x.gioiTinh;
+            this.dangNguoi = x.dangNguoi;
+        }
+        public override string anhNguoi()
+        {
+            return $"Gioi tinh:  {this.gioiTinh}\nKhuon mat:  {this.khuonMat} \nThe hinh: {this.dangNguoi}";
+        }
+        //Tiến
+        public override string hanhDong(hanhDongNV hanhDongCuaKhach)
+        {
+            return hanhDongCuaKhach();
+        }
+        
+    }
+}
---
+++ DoAnCuoiKi/DoAnCuoiKi/NhanVien.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnCuoiKi
+{
+    public enum ChucVu
+    {
+        NhanVien,
+        TruongNhom,
+        QuanLy
+    }
+    // check lai xem đúng code mày kh
+    public class NhanVien : Nguoi
+    {
+        public string theNhanVien { get; }
+        public ChucVu chucVu { get; }
+        public NhanVien():base()
+        {
+            this.theNhanVien = null;
+            this.chucVu = ChucVu.NhanVien;
+        }
+        public NhanVien(string khuonMat, GioiTinh gioiTinh, string dangNguoi, int theXe, string maNV,ChucVu chucVu) :base(khuonMat,gioiTinh,dangNguoi)
+        {
+            this.chucVu = chucVu;
+            this.theNhanVien = maNV;
+        }
+        public NhanVien(NhanVien x)
+        {
+            this.theNhanVien = x.theNhanVien;
+            this.chucVu = x.chucVu;
+            this.khuonMat = x.khuonMat;
+            this.gioiTinh = x.gioiTinh;
+            this.dangNguoi = x.dangNguoi;
+        }
+
+        public override string hanhDong(hanhDongNV hanhDongCuaNV)
+        {
+            return hanhDongCuaNV();
+        }
+        public override string anhNguoi()
+        {
+            return $"The deo: {this.theNhanVien}\nGioi tinh:  {this.gioiTinh}\nKhuon mat:  {this.khuonMat} \nThe hinh: {this.dangNguoi}";
+        }
+        public static string quetDon()
+        {
+            return "Quet don, ve sinh bai giu xe";
+        }
+        public static string kiemTraBaiXe()
+        {
+            return "Tuan tra bai giu xe";
+        }
+        public static string trucBan()
+        {
+            return "Tien hanh soat the xe ra va cap the cho xe vao";
+        }
+        public static string moCua()
+        {
+            return "Mo cua bai giu xe";
+        }
+        public static string dongCua()
+        {
+            return "Dong cua bai giu xe";
+        }
+        public static string nghiNgoi()
+        {
+            return "Nghi ngoi";
+        }
+    }
+}
